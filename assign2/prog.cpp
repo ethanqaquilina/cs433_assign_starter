f65b586af4ffb4ae4e21d3c277d7d6c055d28825
Ethan Aquilina <68577966+ethanqaquilina@users.noreply.github.com>
2025-02-28 22:39:43 -0800

    Added  directional file stream

--- assign2/prog.cpp
+++ assign2/prog.cpp
@@ -14,7 +14,6 @@
 #include <iostream>
 #include <fcntl.h>
 #include <cstring>
-#include <unistd.h>
 #include <sys/wait.h>
 
 using namespace std;
@@ -26,11 +25,10 @@ using namespace std;
  *
  * @param command
  * @param args
- * @return int
+ * @return int Number of arguments parsed
  */
 int parse_command(char command[], char *args[])
 {
-    // TODO: implement this function
     int count = 0;
     char *pointer = strtok(command, " ");
 
@@ -43,9 +41,6 @@ int parse_command(char command[], char *args[])
     args[count] = NULL;
     return count;
 }
-
-// TODO: Add additional functions if you need
-
 /**
  * @brief The main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
  * @param argc The number of arguments
@@ -54,11 +49,10 @@ int parse_command(char command[], char *args[])
  */
 int main(int argc, char *argv[])
 {
-    char command[MAX_LINE];       // the command that was entered
-    char *args[MAX_LINE / 2 + 1]; // hold parsed out command line arguments
-    int should_run = 1;           /* flag to determine when to exit program */
-
-    // TODO: Add additional variables for the implementation.
+    char command[MAX_LINE];           // The command entered by the user
+    char last_command[MAX_LINE] = ""; // Store last executed command
+    char *args[MAX_LINE / 2 + 1];     // Holds parsed out command line arguments
+    int should_run = 1;               // Flag to determine when to exit program
 
     while (should_run)
     {
@@ -67,28 +61,41 @@ int main(int argc, char *argv[])
 
         // Read the input command
         fgets(command, MAX_LINE, stdin);
-        command[strcspn(command, "\n")] = '\0';
+        command[strcspn(command, "\n")] = '\0'; // Remove newline character
+
+        // Check if user typed "!!"
+        if (strcmp(command, "!!") == 0)
+        {
+            if (strlen(last_command) == 0)
+            {
+                printf("No command history found.\n");
+                continue;
+            }
+            else
+            {
+                printf("%s\n", last_command);  // Print the last command
+                strcpy(command, last_command); // Copy last command to command buffer
+            }
+        }
+        else
+        {
+            strcpy(last_command, command); // Store command in history before parsing
+        }
 
         // Check if command should run in the background
         bool background = false;
-        if (strcspn(command, "&") != -1)
+        int len = strlen(command);
+
+        if (len > 0 && command[len - 1] == '&') // If '&' is the last character
         {
             background = true;
-            command[strcspn(command, "&")] = '\0'; // Remove "&" from args
+            command[len - 1] = '\0'; // Remove "&" from command
         }
 
         // Parse the input command
         int num_args = parse_command(command, args);
         if (num_args == 0)
             continue; // Ignore empty input
-
-        // TODO: Add your code for the implementation
-        /**
-         * After reading user input, the steps are:
-         * (1) fork a child process using fork()
-         * (2) the child process will invoke execvp()
-         * (3) parent will invoke wait() unless command included &
-         */
 
         // Check for "exit" command
         if (strcmp(args[0], "exit") == 0)
@@ -97,37 +104,75 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        if (should_run)
-        {
-            // Create Child Process
-            pid_t pid = fork();
+        // Check for input/output redirection
+        int fd;
+        bool redirect_output = false;
+        bool redirect_input = false;
+        char *filename = NULL;
 
-            // Check if error occured
-            if (pid < 0)
-            {
-                perror("Forked Failed");
-                return 1;
+        for (int i = 0; i < num_args; i++)
+        {
+            if (strcmp(args[i], ">") == 0)
+            { // Output Redirection
+                redirect_output = true;
+                filename = args[i + 1];
+                args[i] = NULL; // Remove ">" and filename from args
+                break;
             }
-            else if (pid == 0)
-            { // Child Runs
-                // Invoke execvp
-                if (execvp(args[0], args) == -1)
+            else if (strcmp(args[i], "<") == 0)
+            { // Input Redirection
+                redirect_input = true;
+                filename = args[i + 1];
+                args[i] = NULL; // Remove "<" and filename from args
+                break;
+            }
+        }
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid < 0)
+        {
+            perror("Fork failed");
+            return 1;
+        }
+        else if (pid == 0)
+        {
+            if (redirect_output)
+            { // Handle output redirection
+                fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (fd < 0)
                 {
-                    perror("Command not found");
+                    perror("Error opening file for output redirection");
                     exit(1);
                 }
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
             }
-            else
-            { // Parent runs
-                if (!background)
+            else if (redirect_input)
+            { // Handle input redirection
+                fd = open(filename, O_RDONLY);
+                if (fd < 0)
                 {
-                    wait(NULL);
-                    printf("Child Complete");
+                    perror("Error opening file for input redirection");
+                    exit(1);
                 }
-                else
-                {
-                    continue;
-                }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+            }
+
+            // Child process
+            if (execvp(args[0], args) == -1)
+            {
+                perror("Command not found");
+                exit(1);
+            }
+        }
+        else
+        { // Parent process
+            if (!background)
+            {
+                wait(NULL);
             }
         }
     }
